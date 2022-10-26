0b804a9224506e7524b89df3bada31713a89303e
Cooper <dancersoftaranto@gmail.com>
2022-10-26 16:26:31 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
 import Nav from "./components/nav/Nav";
 import NewsDetails from "./components/newsdetails/NewsDetails";
+import NewsList from "./components/newsList/NewsList";
 
 const App = () => {
   return (
     <>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="/:title" element={<NewsDetails />} />
+        <Route index element={<Home />} />
+        <Route path="newslist" element={<NewsList />}>
+          <Route path=":title" element={<NewsDetails />} />
         </Route>
       </Routes>
       <Footer />
---
+++ src/components/newsList/NewsList.jsx
@@ -0,0 +1,66 @@
+// import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { v1 as uuid } from "uuid";
+
+import ListStyle from "./newslist.module.css";
+
+const NewsList = () => {
+  const navigate = useNavigate();
+  const [news, setNews] = useState([]);
+
+  const getNews = async () => {
+    const apiKey = "L6k85Ai0gDrPx60p8q6aAyEBBNkqjD8D";
+    const apiAdress =
+      "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=";
+    const url = `${apiAdress}${apiKey}`;
+
+    try {
+      const res = await fetch(url);
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+      const { results } = data;
+      setNews(results);
+      console.log(news);
+    } catch (error) {
+      console.log("Api don't work");
+    }
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <div>
+      <h1>Headlines..</h1>
+      <div className={ListStyle.buttons}>
+        {news.map((item) => {
+          const { title } = item;
+          return (
+            <button
+              onClick={() => navigate(`${title}`, { state: item })}
+              key={uuid()}
+            >
+              {title}
+            </button>
+            // <Link
+            //   key={uuid()}
+            //   to={{
+            //     pathname: `${title}`,
+            //     state: { item },
+            //   }}
+            // >
+            //   {title}
+            // </Link>
+          );
+        })}
+      </div>
+
+      <Outlet />
+    </div>
+  );
+};
+
+export default NewsList;
